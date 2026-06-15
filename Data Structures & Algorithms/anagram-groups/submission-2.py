b95cd1633957b0d939c6149b9188fcb15c81079a
Parrv <parrvr.16@gmail.com>
2026-06-15 15:36:37 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #Use a default dict as you can store multiple values for a given key
+        
+        anagram_dict = defaultdict(list)
+
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            #By default, a default dictionary creates an empty list
+            #So if the sorted word isn't there, the word (value) will still be appended
+            #with 
+            anagram_dict[sorted_word].append(word)
+        
+        return list(anagram_dict.values())
+
+            
+      
+        
+                
+
+    
+
+        
