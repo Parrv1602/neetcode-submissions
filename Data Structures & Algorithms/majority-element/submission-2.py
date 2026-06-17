8a8b54ec465ae6bdb11aa41945e3f72f3739c6d9
Parrv <parrvr.16@gmail.com>
2026-06-17 13:54:16 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        '''
+        My solution:
+        dictionary = {}
+        for num in nums:
+            if num not in dictionary.keys():
+                dictionary[num] = 1
+            else:
+                dictionary[num] += 1
+        
+        largest_num = 0
+        highest_frequency = 0
+        for num, frequency in dictionary.items():
+            if frequency > highest_frequency:
+                highest_frequency = frequency
+                largest_num = num
+
+        return largest_num
+        '''
+
+        #Optimised solution
+        sorted_nums = sorted(nums)
+        #// = Floor division (divide and return the closest integer)
+        return sorted_nums[len(nums)//2]
